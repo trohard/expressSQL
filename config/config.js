52c5cbcca15c698bb5e0a5c3bd2ba0f826982f64
Harding <tdh@salina.k-state.edu>
2019-11-04 10:58:50 -0600

    Add Sequelize and pg packages and configuration

---
+++ config/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "development": {
+    "username": "xgfdtesf",
+    "password": "3rkg-togMqSlHM-tdTFaRoc_25uFI6DJ",
+    "database": "xgfdtesf",
+    "host": "salt.db.elephantsql.com",
+    "dialect": "postgres",
+    "port": 5432,
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  }
+}
